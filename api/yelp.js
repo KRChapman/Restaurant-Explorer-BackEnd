ea897a19cd0f5337012e7f6331d83805322773fc
Kyle Chapman <splash004@gmail.com>
2020-01-10 17:01:29 -0800

    set up api call and clean up

--- api/yelp.js
+++ api/yelp.js
@@ -6,7 +6,6 @@ const yelpApi =  function  (queries, displayLimit, cb){
   let index = 0;
 
   return new Promise(function (resolve, reject) {
-
     iterateApiCalls(queries, index, yelpData);
     function iterateApiCalls(queries, index, yelpData)  {
 
@@ -18,20 +17,15 @@ const yelpApi =  function  (queries, displayLimit, cb){
       // 1: { id: "rkiqLiA9mvM-5ULFxx6TUg", alias: "nickos-seattle", name: "Nicko's", image_url: "https://s3-media0.fl.yelpcdn.com/bphoto/VXmkdiYR_zmdBDwVB3F7Gg/o.jpg", is_closed: true, … }
 
       if (index < displayLimit) {//NEED TO pass from display limit in LAYOUT COMPONENT!!!
- 
         var i = index;
         var urlBusinesses = `https://api.yelp.com/v3/businesses/matches?name=${queries[i].name}&address1=${queries[i].address}&city=${queries[i].city}&state=${queries[i].state}&country=${queries[i].country}`;
-        var urlPhone = `https://api.yelp.com/v3/businesses/search/phone?phone=${queries[i].phoneNumber}`
-
+        var urlPhone = `https://api.yelp.com/v3/businesses/search/phone?phone=${queries[i].phoneNumber}`;
         var urlMatch;
         var request = {
           method: 'GET',
-          //  url: urlPhone,
           headers: { "Authorization": "Bearer gqw4k3JJGYyUVrE5fvmaOBd9YerLDsSJxXtBykLWy3U1226XfsGL4gDIq0ARBRsoiuJGN66bEh0ozpxleHGcC3rB8uncvLSg8r0gVCaw8rYDrBXr3PaSaVF1MNnPW3Yx" }
         }
-
         const apiEndpointToFetch = queries[i].phoneNumber != null ? yelpPhone : yelpBusiness;
-        console.log("queriesqueries", queries);
 
         apiEndpointToFetch();
       }
@@ -39,64 +33,51 @@ const yelpApi =  function  (queries, displayLimit, cb){
         resolve(yelpData)
       }
 
-
       function yelpPhone() {
         request.url = urlPhone
         Promise.resolve(
           apiRequest(urlPhone, request),
-
         ).then((response) => {
-          return response.json()
+         let jsons = response.json()
+          return jsons;
         })
           .then((data) => {
             let yelp = {
               yelp: data
-
             }
-
             if (data.businesses.length <= 0) {
               yelpBusiness();
             }
             else {
               index = index + 1;
               yelpData.push(yelp);
-              // debugger;
               iterateApiCalls(queries, index, yelpData)
             }
-
           }).catch(e => {
+            //console.log('yelpPhone', e);
             yelpBusiness()
           })
-
-
       }
 
       function yelpBusiness() {
         request.url = urlBusinesses
         Promise.resolve(
-          apiRequest(urlBusinesses, request),
-
+          apiRequest(urlBusinesses, request)
         ).then((response) => {
           return response.json()
         })
           .then((data) => {
-
-
             dataId = data.businesses[0].id
-            //queries, index, yelpData
             yelpDetails(dataId)
           }).catch(e => {
-            //  console.log('eeee', e);
+           // console.log('yelpBusiness', e);
             let yelp = {
               yelp: null
-
             }
             index = index + 1;
             yelpData.push(yelp);
-
             iterateApiCalls(queries, index, yelpData)
           })
-
       }
 
       function yelpDetails(id) {
@@ -112,34 +93,22 @@ const yelpApi =  function  (queries, displayLimit, cb){
           .then((data) => {
             let yelp = {
               yelp: data
-
             }
-
             yelpData.push(yelp);
             index = index + 1;
             iterateApiCalls(queries, index, yelpData)
           }).catch(e => {
-            console.log('eeee', e);
+            //console.log('yelpDetails', e);
             let yelp = {
               yelp: null
-
             }
-
             index = index + 1;
             yelpData.push(yelp);
-
             iterateApiCalls(queries, index, yelpData)
           })
       }
     }
-})
-
-//
-  
-  
- // return getYelpData;
- // return  getYelpData(queries, index, yelpData);
-  
+  })
 
 
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,5 @@
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 const { yelpApi} = require('./../api/yelp');
 const app = express();
 app.use(function (req, res, next) {
@@ -26,177 +22,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/yelp', async (req, res) => {
- 
   const displayLimit = req.body.displayLimit;
-  const iterateApiCalls = (queries, index, yelpData ) => {
-  
-    // SAME PHONE NUMBER ONE IS CLOSED !!!
-    // maybe iterate find not closed one then maybe algo that finds closes name based on query
-    // yelp:
-    // businesses: Array(2)
-    // 0: { id: "SQKRGzDa7qd8qHb13Z2-Hg", alias: "delinomore-seattle", name: "DeliNoMore", image_url: "https://s3-media0.fl.yelpcdn.com/bphoto/H1p6njRYuEULDluuwEbiNw/o.jpg", is_closed: false, … }
-    // 1: { id: "rkiqLiA9mvM-5ULFxx6TUg", alias: "nickos-seattle", name: "Nicko's", image_url: "https://s3-media0.fl.yelpcdn.com/bphoto/VXmkdiYR_zmdBDwVB3F7Gg/o.jpg", is_closed: true, … }
-
-    if (index < displayLimit) {//NEED TO pass from display limit in LAYOUT COMPONENT!!!
-      if (index === 0){
-      //  queries[index].phoneNumber = '+15555555'
-      }
-      var i = index;
-      var urlBusinesses = `https://api.yelp.com/v3/businesses/matches?name=${queries[i].name}&address1=${queries[i].address}&city=${queries[i].city}&state=${queries[i].state}&country=${queries[i].country}`;
-      var urlPhone = `https://api.yelp.com/v3/businesses/search/phone?phone=${queries[i].phoneNumber}`
-
-      var urlMatch;
-      var request = {
-        method: 'GET',
-        //  url: urlPhone,
-        headers: { "Authorization": "Bearer gqw4k3JJGYyUVrE5fvmaOBd9YerLDsSJxXtBykLWy3U1226XfsGL4gDIq0ARBRsoiuJGN66bEh0ozpxleHGcC3rB8uncvLSg8r0gVCaw8rYDrBXr3PaSaVF1MNnPW3Yx" }
-      }
-
-      const apiEndpointToFetch = queries[i].phoneNumber != null ? yelpPhone : yelpBusiness;
-     // console.log("queriesqueries", queries);
-
-      apiEndpointToFetch();
-    }
-    else {
-
-     // res.send(yelpData);
-      return yelpData
-    }
-
-
-    function yelpPhone(){
-      request.url = urlPhone
-      Promise.resolve(
-        apiRequest(urlPhone, request),
-
-      ).then((response) => {
-       let json = response.json();
-        return json;
-      })
-        .then((data) => {
-          let yelp = {
-            yelp: data
-
-          }
-
-          if (data.businesses.length <= 0){
-            yelpBusiness();
-          } 
-          else{
-            index = index + 1;
-            yelpData.push(yelp);
-           // debugger;
-            iterateApiCalls(queries, index, yelpData)
-          }
-       
-        }).catch(e => {
-          yelpBusiness()
-        })
-
-
-    }
-
-    function yelpBusiness(){
-      request.url = urlBusinesses
-      Promise.resolve(
-        apiRequest(urlBusinesses, request),
-
-      ).then((response) => {
-        return response.json()
-      })
-        .then((data) => {
-
-       
-          dataId = data.businesses[0].id
-       //queries, index, yelpData
-          yelpDetails(dataId)
-        }).catch(e => {
-        //  console.log('eeee', e);
-          let yelp = {
-            yelp: null
-
-          }
-          index = index + 1;
-          yelpData.push(yelp);
-     
-          iterateApiCalls(queries, index, yelpData)
-        })
-
-    }
-
-    function yelpDetails(id){
-      const urlDetails = `https://api.yelp.com/v3/businesses/${id}`
-     
-      request.url = urlDetails
-      Promise.resolve(
-        apiRequest(urlDetails, request)
-
-      ).then((response) => {
-        return response.json()
-      })
-        .then((data) => {
-          let yelp = {
-            yelp: data
-
-          }
-
-          yelpData.push(yelp);
-          index = index + 1;
-          iterateApiCalls(queries, index, yelpData)
-        }).catch(e => {
-          console.log('eeee', e);
-          let yelp = {
-            yelp: null
-
-          }
-
-          index = index + 1;
-          yelpData.push(yelp);
- 
-          iterateApiCalls(queries, index, yelpData)
-        })
-    }
-
-  }
-
- // const yelpData = await
-  // iterateApiCalls(req.body.data, 0, [])
-
   const yelpData = await yelpApi(req.body.data, displayLimit, res);
-  // const yelpDataS = await yelpData
- console.log('yelpDatayelpData', yelpData);
+ 
   res.send(yelpData);
-  function cb(yelpDataS){
-    res.send(yelpDataS);
-  }
- // const getYelpData = yelpApi(req.body.data, displayLimit, res)
-
-  // const getYelpData = yelpApi(req.body.data, displayLimit)
-  // const yelpData = await getYelpData();
-  // console.log('yelpDatayelpData', yelpData);
-  // res.send(yelpData);
 });
 
 
-async function apiRequest(endpoint, request) {
-  const response = await fetch(endpoint, {
-    ...request,
-    //  headers: { ...request.headers, 'Content-Type': 'application/json'}
-  });
-
-  if (response.status < 200 || response.status >= 300) {
-    const error = new Error(response.statusText);
-    error.response = response;
-    error.status = response.status;
-    throw error;
-  }
-
-  return response;
-}
-
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
